4f5b9345f947d03f20dc29941ac794509a4f2aa0
Santiago Grinblat <grinblatsantiago@gmail.com>
2023-11-17 00:12:50 -0300

    validaciones de formulario de login

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,10 +10,17 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent {
 
+  contactForm!: FormGroup;
   username: string = '';
   password: string = '';
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, private readonly fb: FormBuilder) {
+    this.contactForm = fb.group({
+      formularioNombreUsuario: ['', [Validators.required, Validators.minLength(3)]],
+      formularioPasswordUsuario: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
   onSubmit() {
     this.authService.loginAndRedirect(this.username, this.password).subscribe(
       // Manejar cualquier resultado o error si es necesario
